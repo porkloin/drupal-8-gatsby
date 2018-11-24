7b59db0d862f26b1c90a59008f997f8aa9c0b7f5
Ryan Bateman <rybateman@gmail.com>
2018-11-24 13:37:12 -0900

    Step 6: smart blog component

--- src/components/BlogTeaser.js
+++ src/components/BlogTeaser.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BlogTeaser = ( {title, summary} ) => (
   <div className="blog--teaser">
     <h2>{title}</h2>
-    <p>{summary}</p>
+    <p dangerouslySetInnerHTML={{__html: summary}} />
   </div>
 )
 
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,40 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import BlogTeaser from '../components/BlogTeaser.js'
 
-const BlogPage = () => (
+const BlogPage = ( { data } ) => (
   <Layout>
     <h1>Blog</h1>
-    <BlogTeaser
-      title='My first great blog post'
-      summary="I have so many great thoughts, I can't help but share them!"
-    />
+    {data.allNodeArticle.edges.map((post) => (
+      <BlogTeaser
+        key={post.node.id}
+        title={post.node.title}
+        summary={post.node.body.summary.length > 0 ? post.node.body.summary : post.node.body.processed.substring(0, 300)}
+      />
+    ) )}
   </Layout>
 )
 
+export const query = graphql`
+  query BlogPageQuery {
+    allNodeArticle {
+      edges {
+        node {
+          id
+          title
+          body {
+            processed
+            summary
+          }
+        }
+      }
+    }
+  }
+`
+
 export default BlogPage
+
+
+
